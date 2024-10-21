47f49ff528bd4ef686947c469679aaa0a4e3580c
Camila Zúñiga <cfzunigamunoz@gmail.com>
2024-10-21 01:50:28 -0300

    add postman-collection and set configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormsModule } from './forms/forms.module';
+import { APP_GUARD } from '@nestjs/core';
 import { ResponsesModule } from './responses/responses.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST || 'localhost',
+      host: process.env.DATABASE_HOST || 'mysql',
       port: +process.env.DATABASE_PORT || 3306,
-      username: process.env.DATABASE_USER || 'root',
+      username: process.env.DATABASE_USER || 'user',
       password: process.env.DATABASE_PASSWORD || 'root',
       database: process.env.DATABASE_NAME || 'formsdb',
       autoLoadEntities: true,
@@ -17,6 +18,6 @@ import { ResponsesModule } from './responses/responses.module';
     }),
     FormsModule,
     ResponsesModule,
-  ],
+  ]
 })
 export class AppModule {}
